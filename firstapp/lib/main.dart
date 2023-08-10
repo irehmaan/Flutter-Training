88950d3ec709ef6e3fcca07c4c08ad9d87102c01
Imdadul Rehmaan <rehmaankhan287@gmail.com>
2023-08-10 17:57:15 +0530

    added .gitattributes

--- firstapp/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import '/pages/home.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(home: Home() // home: SafeArea(child: Text("Hello")),
-      ));
-}
--- groceryapp/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:groceryapp/pages/grocery.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    home: ShoppingApp(), // home: SafeArea(child: Text("Hello")),
-  ));
-}
